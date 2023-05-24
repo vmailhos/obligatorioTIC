5f90ee8f31f8d66ca6118cc1581d4b4520108190
mariagracianin <mariagracia.nin@gmail.com>
2023-05-24 19:16:48 -0300

    borro cosas inutiles

--- minish-func.c
+++ minish-func.c
@@ -80,48 +80,5 @@ int linea2argv(char *linea, int argc, char **argv){
         }
         i++;
     }
-    
-    printf("%d\n",wordcount);
-    printf("%s\n", argv[0]);
-    printf("%s\n", argv[1]);
-    printf("%s\n", argv[2]);
-
     return wordcount;
 }
-
-/* j = 0;
-        while(linea[i]==' '||linea[i]=='\t'){ //se saltea los primeros espacios si hay
-            i++; 
-        }
-        while(linea[i]!=' '||linea[i]!='\t'){ //se saltea los primeros espacios si hay
-            texto[j]=linea[i];
-            i++;
-            j++;
-        }
-        char* info = strdup_or_exit(texto);
-        contador++;
-        argv[contador] = info;*/
-
-char *get_word(char *line, int position, char *word_to_print){
-    int i=0; 
-    int x=0;
-    while(line[i]==' '||line[i]=='\t'){ //se saltea los primeros espacios si hay
-            i++; 
-    }
-    for (int j=0; j<position-1; j++){ //se saltea palabras y sus espacios dsp
-        while(line[i]!=' ' && line[i]!='\0' && line[i]=='\t'){
-            i++;
-        }
-        while(line[i]==' '&&line[i]!='\0' && line[i]=='\t'){
-            i++; 
-        }
-    }
-    while(line[i]!=' ' && line[i]!='\0' && line[i]=='\t'){
-        word_to_print[x]=line[i]; 
-        i++;
-        x++;
-    }
-    word_to_print[x]='\0';//no se si tiene del todo sentido porq lo cree con un tamaño max
-    return word_to_print;
-}
-
