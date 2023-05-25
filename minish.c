cb4c886223e1dba8796c5ba824dee732cb838ea2
rosinavarela <rjvarela@correo.um.edu.uy>
2023-05-25 12:59:57 -0300

    main

--- minish.c
+++ minish.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 #include "wrappers.h"
 #include "minish.h"
 
+#define PATH_MAX 4096
 
-int main(int argc, char *argv[]){
-    char *linea = "aaaa bbbb ccc       \n";
-    linea2argv(linea, argc, argv);
+
+int main(void){ //hay que manejar errores tambien 
+    uid_t uid = getuid();
+    struct passwd *pwd = getpwuid(uid);
+    char *username= pwd->pw_name;
+    char path[PATH_MAX];    
+    char *directorio = getcwd(path, sizeof(path));
+
+    while (1){
+        fprintf(stderr, "(minish) %s:%s\n",username, directorio);
+    }
+    //char *linea = "aaaa bbbb ccc       \n";
+    //linea2argv(linea, argc, argv);
     /*while(1){
         fprintf(stderr, "Mensaje de prompt en standard error: \n");   //para borrar
     }*/
