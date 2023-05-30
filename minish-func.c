1d8797dcc420cea10af773bb0f1ce36f592e0eb8
vmailhos <vale.mailhos@gmail.com>
2023-05-30 14:33:35 -0300

    comandos externos

--- minish-func.c
+++ minish-func.c
@@ -25,20 +25,43 @@ int ejecutar (int argc, char ** argv){
 }
 
 int externo (int argc, char ** argv){
-    //No lo termine!
+
     pid_t pid = fork();
 
     if (pid == -1) {
-        printf("Error.\n");
+        perror("Error en el fork.\n");
+        return -1;
+
     } else if (pid == 0) {
-        // Proceso hijo
+        execvp(argv[0], argv);
+        perror("Error en el execvp");
+        return -1;
 
     } else {
-        // Proceso padre
         int status;
-    }
-}
+        waitpid(pid, &status, 0); //espera a que termine el hijo para tener el status
+       
+        //OPCIONES:
+        //Proceso hijo termino correctamente
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            return exit_status;
+               
+        //Proceso hijo termino por una senal
+        }else if (WIFSIGNALED(status)) {
+            int signal_number = WTERMSIG(status);
+            printf("El proceso hijo terminó debido a la señal %d\n", signal_number);
+       
+        //Proceso hijo se detuvo por una senal
+        } else if (WIFSTOPPED(status)) {
+            int signal_number = WSTOPSIG(status);
+            printf("El proceso hijo se detuvo debido a la señal %d\n", signal_number);
+        }
 
+    }
+
+    return -1;
+}
 
 
 int linea2argv(char *linea, int argc, char **argv) {  
