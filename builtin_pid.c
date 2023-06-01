05242b4b24d20c9be54a817d3de55ba4145434a3
vmailhos <vale.mailhos@gmail.com>
2023-06-01 12:13:14 -0300

    pid

--- builtin_pid.c
+++ builtin_pid.c
@@ -2,19 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
  
-#include <unistd.h>
 #include <sys/types.h>
-#include <pwd.h>
-#include <signal.h>
-#include <errno.h>
+#include <unistd.h>
 
 #include "minish.h"
 #include "wrappers.h"
 
 
 int builtin_pid(int argc, char **argv) {
+    
     pid_t pid = getpid();
-    //nunca puede retornar null? preguntar
-    printf("Process ID: %d\n", getpid());
+    pid_t ppid = getppid();
+  
+    printf("pid: %d, ppid: %d\n", getpid(), getppid());
+
     return 0;
 }
