1256c0b3cecf612f189b61113a8eb305f0749d80
vmailhos <vale.mailhos@gmail.com>
2023-06-01 11:57:11 -0300

    pid

--- builtin_pid.c
+++ builtin_pid.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+ 
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <signal.h>
+#include <errno.h>
 
 #include "minish.h"
 #include "wrappers.h"
