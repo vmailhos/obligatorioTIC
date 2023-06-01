fc30a9afd3c015b51a34294621dd362656aac3dc
vmailhos <vale.mailhos@gmail.com>
2023-06-01 12:36:11 -0300

    uid

--- builtin_uid.c
+++ builtin_uid.c
@@ -8,11 +8,10 @@
 #include "wrappers.h"
 
 //no puede tener errores
+//Uid: 202320, Name: vmailhos, Info = 'Valentina MAILHOS URIOSTE,,,'
 int builtin_uid (int argc, char ** argv){
     uid_t uid = getuid();
     struct passwd *pwd = getpwuid(uid);
-    printf("User ID: %d\n", uid);
-    printf("Username: %s\n", pwd->pw_name);
-
+    printf("Uid: %d, Name: %s, Info: %s\n", uid, pwd->pw_name, pwd->pw_gecos);
     return 0;
 }
