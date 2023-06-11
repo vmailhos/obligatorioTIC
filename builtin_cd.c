71445bf348c02bfbf0d93141cae4b217f91b63d9
rosinavarela <rjvarela@correo.um.edu.uy>
2023-06-11 19:23:06 -0300

    cd

--- builtin_cd.c
+++ builtin_cd.c
@@ -6,16 +6,13 @@
 #include "minish.h"
 #include "wrappers.h"
 
-
 //arreglar sprintf y el main como lo cambio gerardo a mery
 //controlar errores de si te da mas de un argv en todas las funciones
 //no funciona del todo bien a veces cambias de directorio y el anterior se mantiene igual
 int builtin_cd (int argc, char ** argv){
     char *home_dir = getenv("HOME");
     char *next_dir;
-    static char *prev_dir;
-    static char *actual_dir;
-    actual_dir= getenv("PWD");
+    char *copy_of_PWD; 
 
     if (argc>2){
         perror("Error, numero incorrecto de argumentos"); //me tira undefined error 0 
@@ -24,48 +21,57 @@ int builtin_cd (int argc, char ** argv){
 
     //cuando solo está el comando
     else if (argc==1 && home_dir != NULL){
-        prev_dir=actual_dir;
-        chdir(home_dir);
-        setenv("PWD", home_dir, 1);
-        actual_dir=home_dir;
-        if (prev_dir!=NULL){
-            printf("prev: %s \n", prev_dir); 
-            printf("actual:%s \n", actual_dir); 
+        setenv("OLDPWD", getenv("PWD"), 1);
+        if(chdir(home_dir)==-1){
+            perror("No se pudo cambiar a directorio HOME \n");
+            return 1;
+        }
+        if (setenv("PWD", home_dir, 1)==-1){
+            perror("No se pudo cambiar a directorio HOME\n");
+            return 1;
         }
     }
 
     //cuando hay dos argumentos. 
-    else if (argv[1][0]=='-' && prev_dir != NULL){    
-        chdir(prev_dir);
-        setenv("PWD", prev_dir, 1);
-        prev_dir=actual_dir;
-        actual_dir=getenv("PWD");
-        if (prev_dir!=NULL){
-            printf("prev: %s \n", prev_dir); 
-            printf("actual:%s \n", actual_dir); 
+    else if (strcmp(argv[1],"-")==0 && getenv("OLDPWD") != NULL){    
+
+        //errores
+        if((chdir(getenv("OLDPWD")))==-1){
+            perror("No se pudo cambiar a directorio anterior\n");
+            return 1;
         }
+        copy_of_PWD=strdup_or_exit(getenv("PWD"));
+        if (setenv("PWD", getenv("OLDPWD"), 1)==-1){
+            perror("No se pudo cambiar a directorio anterior\n");
+            return 1;
+        }
+        if (setenv("OLDPWD", copy_of_PWD, 1)==-1){
+            perror("No se pudo cambiar a directorio anterior\n");
+            return 1;
+        }
+        free(copy_of_PWD);
     }
     
     else { 
         // Cambiar al directorio especificado
         next_dir= argv[1];
-        if (chdir(next_dir) == 0) {
-            char absolute_path[1024]; //usar getcwd
-            char *result = realpath(next_dir, absolute_path);
-            if (result != NULL) {
-                prev_dir=actual_dir;
-                setenv("PWD", absolute_path, 1);
-                actual_dir=getenv("PWD");
-            }
+        if (chdir(next_dir)==-1) {
+            perror("No se pudo cambiar a directorio indicado\n");
+            return 1;
+        }
+        char absolute_path[1024]; //usar getcwd
+        char *result = realpath(next_dir, absolute_path);
+            
+        if (result != NULL) {
+            setenv("OLDPWD", getenv("PWD"), 1);
+            setenv("PWD", absolute_path, 1);
         }
         else {
             perror("Directorio no encontrado\n");
             return 1;
         }
-        if (prev_dir!=NULL){                
-            printf("prev: %s \n", prev_dir); 
-            printf("actual:%s \n", actual_dir); 
-        }    
-    return 0;
     }
+
+    return 0;
 }
+
